b58f4dfe4abfd365b7ffff90f2d35ea6a4cf02f3
Foap2023 <Foap2023@BI101-16.epsevg.upc.es>
2024-09-03 13:50:51 +0200

    commit

--- web/includes/header.php
+++ web/includes/header.php
@@ -22,24 +22,28 @@
         $user = $_SESSION['user'];
     };
     ?>
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark ">
+        <div class="container-fluid d-flex align-items-center">
         <?php
             if  (!array_key_exists($soy, $tablaNav)) {
                 echo 'Opción no configurada en barra de NAVegación. Contacte con el Administrador.';
             }else{
         ?>
-            <ul class="navbar-nav  me-auto">
-                <li class="nav-item">
-                    <?php
-                        if  (array_key_exists('a_bienvenida', $tablaNav[$soy])) {
-                            echo '<a class="nav-link" href="bienvenidaCarousel.php">Bienvenida</a>';
-                        }else{
-                            echo '<a class="nav-link not-active" href="bienvenidaCarousel.php">Bienvenida</a>';
-                        }; 
-                    ?>
-                </li>
+        <?php
+            if  (array_key_exists('a_bienvenida', $tablaNav[$soy])) {
+                echo '<a class="navbar-brand" href="bienvenidaCarousel.php">Bienvenida</a>';
+            }else{
+                echo '<a class="navbar-brand not-active" href="bienvenidaCarousel.php">Bienvenida</a>';
+            }; 
+        ?>
+  
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        
+            <ul class="navbar-nav">
                 <li class="nav-item">
                     <?php
                         if  (array_key_exists('a_tiendas', $tablaNav[$soy])) {
@@ -76,55 +80,63 @@
                         };
                     ?>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item me-auto">
                     <?php
                         if  (array_key_exists('a_carrito',$tablaNav[$soy])){
     //                        echo '<a class="nav-link" href="verCarrito.php"><span class="icoCarro">&#128722;</span></a>';
                             echo '<a class="nav-link" href="verCarrito.php"><i class="bi bi-cart"></i></a>';
                         }else{
-                            echo '<a class="nav-link not-active" href="verCarrito.php"><i class="bi bi-cart"></i></a>';
+                            
     //                        echo '<a class="nav-link not-active" href="verCarrito.php"><span class="icoCarro">&#128722;</span></a>';
+                            echo '<a class="nav-link not-active" href="verCarrito.php"><i class="bi bi-cart"></i></a>';
                         };
                     ?>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownUsuario" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Usuario
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li>
+                        <?php
+                            if  (array_key_exists('a_login',$tablaNav[$soy])){
+                                echo ' <a class="dropdown-item" href="usuarios_login.php">Iniciar Session</a>';
+                            }else{
+                                echo ' <a class="dropdown-item not-active" href="usuarios_login.php">Iniciar Session</a>';
+                            };
+                        ?>
+                        </li>
+                        
+                        <li>
+                        <?php
+                            if  (array_key_exists('a_datos',$tablaNav[$soy])){
+                                echo '<a class="dropdown-item" href="usuarios_misdatos.php">Mis datos</a>';
+                            }else{
+                                echo '<a class="dropdown-item not-active" href="usuarios_misdatos.php">Mis datos</a>';
+                            };
+                        ?>
+                        </li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li">
+                        <?php
+                            if  (array_key_exists('a_logout',$tablaNav[$soy])) {
+                                echo '<a class="dropdown-item" href="../includes/logout.php">Logout</a>';
+                            }else{
+                                echo '<a class="dropdown-item not-active" href="../includes/logout.php">Logout</a>';
+                            };
+                        ?>
+                        
+                        </li>
+                    </ul>
+                </li>
             </ul>
+          </div>
         
-        <div class="d-flex">
-            <ul class="navbar-nav  me-auto">
-                <li class="nav-item">
-                    <?php
-                        if  (array_key_exists('a_login',$tablaNav[$soy])){
-                            echo ' <a class="nav-link" href="usuarios_login.php">Iniciar Session</a>';
-                        }else{
-                            echo ' <a class="nav-link not-active" href="usuarios_login.php">Iniciar Session</a>';
-                        };
-                    ?>
-                </li>
-                <li class="nav-item">
-                    <?php
-                        if  (array_key_exists('a_logout',$tablaNav[$soy])) {
-                            echo '<a class="nav-link" href="../includes/logout.php">Logout</a>';
-                        }else{
-                            echo '<a class="nav-link not-active" href="../includes/logout.php">Logout</a>';
-                         };
-                    ?>
-                </li>
-                <li class="nav-item">
-                    <?php
-                        if  (array_key_exists('a_datos',$tablaNav[$soy])){
-                            echo '<a class="nav-link" href="usuarios_misdatos.php">Mis datos</a>';
-                        }else{
-                            echo '<a class="nav-link not-active" href="usuarios_misdatos.php">Mis datos</a>';
-                        };
-                    ?>
-                </li>
-            </ul>
-        </div>
+
         
         <?php
             };
         ?>
-        </div>
         </div>
         </nav>
 </section>
